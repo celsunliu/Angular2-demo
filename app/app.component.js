f37ded17c7a5ad3d218023bbcf2c07bd9b416a5c
Alex <celsunliu@hotmail.com>
2018-08-24 16:51:23 +0800

    add app folder

---
+++ app/app.component.js
@@ -0,0 +1,11 @@
+
+n(app) {
+  app.AppComponent =
+    ng.core.Component({
+      selector: 'my-app',
+      template: '<h1>我的第一个 Angular2 应用</h1>'
+    })
+    .Class({
+      constructor: function() {}
+    });
+})(window.app || (window.app = {}));
